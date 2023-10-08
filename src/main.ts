df0ed49163fa54a50db826e75d77ee55ff546ce2
Jonathan Alvarez <jalva100@ucsc.edu>
2023-10-08 14:11:10 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const clicker = document.createElement("button");
+clicker.innerHTML = "CLICK ON ME PLS 💯🔥🔥👌👌";
+app.append(clicker);
