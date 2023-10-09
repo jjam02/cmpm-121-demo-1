b44b93243f68d6cf9c17c7d870b1e43dcf53c0c7
Jonathan Alvarez <jalva100@ucsc.edu>
2023-10-08 19:34:01 -0700

    step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Jonathan Alvarez game";
 let counter: number = 0;
 let rate: number = 0;
-let upgrade1Cost = 10;
+let upgrade1Cost: number = 10;
+let upgrade1Owned: number = 0;
+let upgrade2Cost: number = 100;
+let upgrade2Owned: number = 0;
+let upgrade3Cost: number = 1000;
+let upgrade3Owned: number = 0;
 
 document.title = gameName;
 
@@ -29,13 +34,22 @@ clicker.addEventListener("click", () => {
 app.append(clicker);
 
 const upgrade1 = document.createElement("button");
-upgrade1.innerHTML = `hamster on wheel (${upgrade1Cost} ⚡) +1 ⚡/sec `;
+upgrade1.innerHTML = `hamster on wheel (${upgrade1Cost.toFixed(
+  2,
+)} ⚡) +1 ⚡/sec `;
 upgrade1.disabled = true;
 upgrade1.addEventListener("click", () => {
-  rate += 1;
+  upgrade1Owned += 1;
+  const upgrade1Data: HTMLTableRowElement = document.getElementById(
+    "upgrade1Row",
+  ) as HTMLTableRowElement;
+  upgrade1Data.cells[1].textContent = `${upgrade1Owned}`;
+  rate += 0.1;
   counter -= upgrade1Cost;
-  upgrade1Cost += 5;
-  upgrade1.innerHTML = `hamster on wheel (${upgrade1Cost} ⚡) +1 ⚡/sec `;
+  upgrade1Cost *= 1.15;
+  upgrade1.innerHTML = `hamster on wheel (${upgrade1Cost.toFixed(
+    2,
+  )} ⚡) +0.1 ⚡/sec `;
   if (counter < upgrade1Cost) {
     upgrade1.disabled = true;
   }
@@ -45,8 +59,54 @@ upgrade1.addEventListener("click", () => {
 
 app.append(upgrade1);
 
+const upgrade2 = document.createElement("button");
+upgrade2.innerHTML = `coal miner (${upgrade2Cost} ⚡) +2 ⚡/sec `;
+upgrade2.disabled = true;
+upgrade2.addEventListener("click", () => {
+  upgrade2Owned += 1;
+  const upgrade2Data: HTMLTableRowElement = document.getElementById(
+    "upgrade2Row",
+  ) as HTMLTableRowElement;
+  upgrade2Data.cells[1].textContent = `${upgrade2Owned}`;
+  rate += 2;
+  counter -= upgrade2Cost;
+  upgrade2Cost *= 1.3;
+  upgrade2.innerHTML = `coal miner (${upgrade2Cost.toFixed(2)} ⚡) +2 ⚡/sec `;
+  if (counter < upgrade2Cost) {
+    upgrade2.disabled = true;
+  }
+
+  rateInfo.innerHTML = `autoclick ⚡${rate.toFixed(2)}/sec`;
+});
+app.append(upgrade2);
+
+const upgrade3 = document.createElement("button");
+upgrade3.innerHTML = `wind turbine (${upgrade3Cost.toFixed(2)} ⚡) +50 ⚡/sec `;
+upgrade3.disabled = true;
+upgrade3.addEventListener("click", () => {
+  upgrade3Owned += 1;
+  const upgrade3Data: HTMLTableRowElement = document.getElementById(
+    "upgrade2Row",
+  ) as HTMLTableRowElement;
+  upgrade3Data.cells[1].textContent = `${upgrade3Owned}`;
+  rate += 50;
+  counter -= upgrade3Cost;
+  upgrade3Cost *= 1.57;
+  upgrade3.innerHTML = `wind turbine (${upgrade3Cost.toFixed(
+    2,
+  )} ⚡) +50 ⚡/sec `;
+  if (counter < upgrade3Cost) {
+    upgrade3.disabled = true;
+  }
+
+  rateInfo.innerHTML = `autoclick ⚡${rate.toFixed(2)}/sec`;
+});
+
+app.append(upgrade3);
+
 const count = document.createElement("div");
 count.innerHTML = `This much power ${counter} ⚡`;
+count.style.fontSize = "50px";
 app.append(count);
 
 setInterval(upgradeChecker, 500);
@@ -56,6 +116,18 @@ function upgradeChecker() {
     upgrade1.disabled = false;
   } else {
     upgrade1.disabled = true;
+  }
+
+  if (counter >= upgrade2Cost) {
+    upgrade2.disabled = false;
+  } else {
+    upgrade2.disabled = true;
+  }
+
+  if (counter >= upgrade3Cost) {
+    upgrade3.disabled = false;
+  } else {
+    upgrade3.disabled = true;
   }
 }
 let frameCount = 0;
